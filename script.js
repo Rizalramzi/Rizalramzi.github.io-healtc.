8c4475ade6686e75ab3f874b4fb748be6180a047
Rizalramzi <rizalramzi0@gmail.com>
2024-09-07 10:44:11 +0700

    modified about, index

--- script.js
+++ script.js
@@ -26,4 +26,41 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   });
 
+
+  document.addEventListener('DOMContentLoaded', function() {
+    // Ambil elemen a#konten
+    const scrollToContentLink = document.querySelector('a[href="#konten"]');
+
+    // Fungsi untuk melakukan smooth scroll
+    function smoothScroll(target) {
+        document.querySelector(target).scrollIntoView({
+            behavior: 'smooth'
+        });
+    }
+
+    // Event listener untuk klik pada elemen a#konten
+    scrollToContentLink.addEventListener('click', function(event) {
+        event.preventDefault(); // Mencegah tindakan default link
+        const targetId = this.getAttribute('href');
+        smoothScroll(targetId);
+    });
+});
   
+
+document.addEventListener('DOMContentLoaded', function() {
+  let currentIndex = 0;
+  const comments = document.querySelectorAll('.comment');
+  const totalComments = comments.length;
+
+  document.getElementById('nextBtn').addEventListener('click', () => {
+    comments[currentIndex].classList.add('hidden');
+    currentIndex = (currentIndex + 1) % totalComments;
+    comments[currentIndex].classList.remove('hidden');
+  });
+
+  document.getElementById('prevBtn').addEventListener('click', () => {
+    comments[currentIndex].classList.add('hidden');
+    currentIndex = (currentIndex - 1 + totalComments) % totalComments;
+    comments[currentIndex].classList.remove('hidden');
+  });
+});
